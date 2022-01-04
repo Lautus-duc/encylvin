a83f24449efbbac746225fff33c0f5b829656185
Lautus-duc <tg3101tg@gmail.com>
2022-01-04 18:52:26 +0100

    amélioration de la page Baobab entête head

--- entete.inc.php
+++ entete.inc.php
@@ -1,22 +1,23 @@
 <div class="row">  
-    <div  class="card-panel green lighten-2">
-      <h1> <a href="/index.php" ><img src="logo_Encylvin.jpg" style="width:70px;height:90px;"> </a>
-        <span class="green-text text-darken-4 "><?php echo "ncylvin" ?></span></h1>
-              
-              <!-- Dropdown Trigger -->
-            <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Menu !</a>
-
-              <!-- Dropdown Structure -->
-            <ul id='dropdown1' class='dropdown-content'>
-              </li><li><a href="#!"><img src="logo_encylvin_menu.jpg" style="width:50px;height:50px;"></a></li>
-              <li><a href="#!">two</a></li>
-              <li class="divider" tabindex="-1"></li>
-              <li><a href="#!">three</a></li>
-              <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-              <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
-            </ul>
-        
-    </div> 
+  <div  class="card-panel green lighten-2">
+    <h1 class="green-text text-darken-4">
+      <a href="/index.php">
+        <img src="logo_Encylvin.jpg" style="width:70px;height:90px;"> 
+      </a>
+      ncylvin
+    </h1>
+    <!-- Dropdown Trigger -->
+    <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Menu !</a>
+    <!-- Dropdown Structure -->
+    <ul id='dropdown1' class='dropdown-content'>
+      <li><a href="#!"><img src="logo_encylvin_menu.jpg" style="width:50px;height:50px;"></a></li>
+      <li><a href="#!">two</a></li>
+      <li class="divider" tabindex="-1"></li>
+      <li><a href="#!">three</a></li>
+      <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
+      <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+    </ul>
+  </div> 
 </div>
 <script type="text/javascript">
    M.AutoInit();
