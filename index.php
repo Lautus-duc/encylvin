c2757808a006212f3199e78a9fc3aa7458fcd348
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 14:19:15 +0100

    modif index

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 
-    <?php include "head.inc.php" ?>
-   
+	<?php include "head.inc.php" ?>
 
-    <body>
-     <?php include "entete.inc.php" ?>
-      <div class="col s12"><h3>Vous trouverez notament des informations sur : </h3></div>
-	    <div class="col s6">
-      <div class="collection">
+
+	<body>
+		<?php include "entete.inc.php" ?>
+		<div class="container">
+			<div class="col s12"><h3>Vous trouverez notament des informations sur : </h3></div>
+			<div class="col s6">
+				<div class="collection">
 					<a href="/index_Baobab.php" class="collection-item">Baobab</a>
 					<a href="/index_Sapin.php" class="collection-item">Sapin</a>
 					<a href="/index_Eucaliptus.php" class="collection-item">Eucaliptus</a>
@@ -18,12 +19,13 @@
 					<a href="/index_Hêtre.php" class="collection-item">Hêtre</a>
 					<a href="/index_Pin.php" class="collection-item">Pin</a>
 				</div>
-      <div class="col s6"></div>
-      </div>
+			</div>
+		</div>
+		</div>
 
-	<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+		<!--JavaScript at end of body for optimized loading-->
+		<script type="text/javascript" src="js/materialize.min.js"></script>
+	</body>
+</html>
     
 
