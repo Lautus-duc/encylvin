6e9881e06e839cb759517a690d3cb8f4e33fc1df
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 13:56:30 +0100

    changement : ginko --> gonkgo + ginkgo

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 					<a href="/index_Sapin.php" class="collection-item">Sapin</a>
 					<a href="/index_Eucaliptus.php" class="collection-item">Eucaliptus</a>
 					<a href="/index_Érable.php" class="collection-item">Érable</a>
-					<a href="/index_Ginko.php" class="collection-item">Ginko</a>
+					<a href="/index_Ginkgo.php" class="collection-item">Ginkgo</a>
 					<a href="/index_Cerisier_du_japon.php" class="collection-item">Cerisier du japon</a>
 					<a href="/index_Chêne.php" class="collection-item">Chêne</a>
 					<a href="/index_Thé_des_jésuites.php" class="collection-item">Thé des jésuites</a>
---
+++ index_Ginkgo.php
@@ -0,0 +1,21 @@
+<html>
+  <?php include "head.inc.php" ?>
+  <body>
+    <?php include "entete.inc.php" ?>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>Le ginkgo</h1></div>
+        <div class="col s4">
+          <img src="ginkgo_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>Le ginkgo est un arbre dioïque dont les individus mâles portent des bouquets de cônes de pollen semblables à des chatons et dont les individus femelles possèdent de longs pédoncules portant à leur extrémité un ovule, nu (sans être enveloppé dans un ovaire), sans pétales.</p>
+          <p>En Chine, certains spécimens de cet arbre auraient une durée de vie excédant les 3 000 ans et plus de 100 individus auraient plus de 1 000 ans.</p>
+          <p>Les Ginkgoales sont apparues au Permien il y a plus de 270 millions d'années et ont prospéré dans le monde entier jusqu’au Mésozoïque, en particulier au Jurassiquen. Mais à la période des glaciations quaternaires, la seule espèce du phylum à subsister est Ginkgo biloba qui ne survécut que dans quelques rares refuges au climat plus doux du Sud de la Chine. Elle est considérée comme une espèce panchroniquen. Ces derniers milliers d’années, elle fut cultivée pour son intérêt ornemental et fut transférée au Japon et en Corée aux alentours du XIIe siècle.</p>
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
+    </div>
+  </body>
+</html>
