b366ec559f69678b52f905884e3d335d87180865
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 14:10:07 +0100

    hetre

--- index_Hêtre.php
+++ index_Hêtre.php
@@ -1,24 +1,21 @@
-  <html>
-    <?php include "head.inc.php" ?>
+<html>
+  <?php include "head.inc.php" ?>
   <body>
-    <div class="row">
     <?php include "entete.inc.php" ?>
-
-      <div class="col s12"><h1><?php echo "L'hêtre" ?></h1></div>
-      <div class="col s12"><h2></h2></div>
-	  <div class="col s6">
-		</div>
-      <div class="col s6"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>L'hêtre</h1></div>
+        <div class="col s4">
+          <img src="hetre_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>Le Hêtre commun, Fagus sylvatica, couramment désigné simplement comme le hêtre est une espèce d'arbres à feuilles caduques, indigène d'Europe, appartenant à la famille des Fagaceae, tout comme le chêne et le châtaignier.</p>
+          <p>Il est l'une des principales essences constitutives des forêts tempérées caducifoliées d'Europe où on peut le trouver en peuplements exclusifs de hêtraies pures ou le plus souvent associé à d'autres espèces majeures dans des forêts feuillues, principalement avec le Chêne rouvre, ou dans des forêts mixtes avec le sapin blanc ou l'Épicéa commun.</p>
+          <p>C'est une essence bioindicatrice d'un climat tempéré humide. Les forestiers en pratiquent de longue date la sylviculture pour produire du bois de futaie principalement destiné à l'ameublement. Il est également utilisé comme source de bois de chauffage, surtout en zone de montagne.</p>
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
     </div>
-
-    <?php echo "Le Hêtre commun, Fagus sylvatica, couramment désigné simplement comme le hêtre est une espèce d'arbres à feuilles caduques, indigène d'Europe, appartenant à la famille des Fagaceae, tout comme le chêne et le châtaignier.
-
-Il est l'une des principales essences constitutives des forêts tempérées caducifoliées d'Europe où on peut le trouver en peuplements exclusifs de hêtraies pures ou le plus souvent associé à d'autres espèces majeures dans des forêts feuillues, principalement avec le Chêne rouvre, ou dans des forêts mixtes avec le sapin blanc ou l'Épicéa commun.
-
-C'est une essence bioindicatrice d'un climat tempéré humide. Les forestiers en pratiquent de longue date la sylviculture pour produire du bois de futaie principalement destiné à l'ameublement. Il est également utilisé comme source de bois de chauffage, surtout en zone de montagne. "?>
-    
-	<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
-
+  </body>
+</html>
