5edfad39a8ed6439df6c90e9dfe85df20a03f2cf
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 13:33:05 +0100

    chene

--- index_Chêne.php
+++ index_Chêne.php
@@ -1,24 +1,25 @@
-  <html>
-    <?php include "head.inc.php" ?>
+<html>
+  <?php include "head.inc.php" ?>
   <body>
-    <div class="row">
     <?php include "entete.inc.php" ?>
-
-      <div class="col s12"><h1><?php echo 'Le chène' ?></h1></div>
-      <div class="col s12"><h2></h2></div>
-	  <div class="col s6">
-		</div>
-      <div class="col s6"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>Le chène</h1></div>
+        <div class="col s4">
+          <img src="chene_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>Chêne est le nom vernaculaire de nombreuses espèces d'arbres et d'arbustes appartenant au genre Quercus, et à certains genres apparentés de la famille des fagacées, notamment Cyclobalanopsis et Lithocarpus.
+          Ce genre, présent dans tout l'hémisphère nord et dont l'aire de répartition s'étend depuis les froides latitudes jusqu'aux zones tropicales de l'Asie et de l'Amérique, comprend à la fois des espèces à feuilles caduques et d'autres à feuilles persistantes.
+          Là où il est naturellement présent dans son aire de répartition, le chêne abrite des centaines d'autres espèces (notamment après sa vie dans son bois mort) ; au point que certains écologues le qualifient de Point chaud de biodiversité.</p>
+          <p>Étymologie et différentes appellations</p>
+          <p>Il n'y a pas de mot indo-européen spécifique au chêne, un arbre qui puise ses noms dans des couches linguistiques plus anciennes. Le nom de chêne renvoie ainsi à quatre étymons différents : l'indo-européen *dreu-, *perkʷus et heyǵ-, et le gaulois *cassanos.</p>
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
     </div>
+  </body>
+</html>
 
-    <?php echo "Chêne est le nom vernaculaire de nombreuses espèces d'arbres et d'arbustes appartenant au genre Quercus, et à certains genres apparentés de la famille des fagacées, notamment Cyclobalanopsis et Lithocarpus.
-Ce genre, présent dans tout l'hémisphère nord et dont l'aire de répartition s'étend depuis les froides latitudes jusqu'aux zones tropicales de l'Asie et de l'Amérique, comprend à la fois des espèces à feuilles caduques et d'autres à feuilles persistantes.
-Là où il est naturellement présent dans son aire de répartition, le chêne abrite des centaines d'autres espèces (notamment après sa vie dans son bois mort) ; au point que certains écologues le qualifient de Point chaud de biodiversité2.
-Étymologie et différentes appellations
-Il n'y a pas de mot indo-européen spécifique au chêne, un arbre qui puise ses noms dans des couches linguistiques plus anciennes. Le nom de chêne renvoie ainsi à quatre étymons différents : l'indo-européen *dreu-, *perkʷus et heyǵ-, et le gaulois *cassanos. "?>
-    
-	<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
 
