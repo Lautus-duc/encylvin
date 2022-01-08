c1b9a3a143e9d00bd09ce8ce236fb94ea0c506f5
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 13:45:45 +0100

    erable

--- index_Érable.php
+++ index_Érable.php
@@ -1,26 +1,24 @@
-  <html>
-    <?php include "head.inc.php" ?>
+<html>
+  <?php include "head.inc.php" ?>
   <body>
-    <div class="row">
     <?php include "entete.inc.php" ?>
-
-      <div class="col s12"><h1><?php echo "L'Érable" ?></h1></div>
-      <div class="col s12"><h2></h2></div>
-	  <div class="col s6">
-		</div>
-      <div class="col s6"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>L'Érable</h1></div>
+        <div class="col s4">
+          <img src="erable_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>L’érable est surtout célèbre pour sa feuille qui est l’emblème du Canada. Certaines espèces sont très connues et principalement utilisées pour produire du sirop d’érable, notamment au Canada et aux États-Unis. D’autres espèces sont plutôt destinées à la menuiserie ou à l’ornementation.</p>
+          <p>Les érables mesurent généralement entre 10 et 45 m de hauteur. Leurs feuilles sont nervurées et arborent leurs couleurs d’automne au mois d’octobre, de l’orange au violet, et leur feuillage est dense. Les fleurs d’érable, également très colorées (rouge, orange, jaune, vert), comportent 4 à 5 pétales très courts, voire inexistants. Après sa floraison, l’érable produit des fruits, qui sont appelés samare. Toujours groupés par deux, ils forment des hélices, ce qui permet à la graine qu’elle contient d’être facilement disséminée par le vent.
+          La feuille d’érable est bien évidemment le symbole du Canada, mais l’érable de manière générale a aussi d’autres symboliques. Il est associé à Phobos, le dieu grec de la peur panique. Son bois était utilisé pour fabriquer les lances de l’armée romaine, et enfin sa fleur symboliserait le sens de l’observation et la réserve.</p>
+          <p> Origine</p>
+          <p>Le genre Érable (Acer) est composé d’arbres de la famille des Sapindacées très répandus dans les zones tempérées fraîches et humides, du sud du Canada et des États-Unis en passant par l’Europe jusqu’à la côte pacifique de l’Asie. L’étymologie est complexe puisqu’en France, l’érable vient de l’ancien français acerabulus, composé d’acer, qui signifie aiguille en latin, et bulus qui viendrait du gaulois abalo pour pomme.</p>
+          
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
     </div>
-
-    <?php echo "L’érable est surtout célèbre pour sa feuille qui est l’emblème du Canada. Certaines espèces sont très connues et principalement utilisées pour produire du sirop d’érable, notamment au Canada et aux États-Unis. D’autres espèces sont plutôt destinées à la menuiserie ou à l’ornementation.
-Origine
-
-Le genre Érable (Acer) est composé d’arbres de la famille des Sapindacées très répandus dans les zones tempérées fraîches et humides, du sud du Canada et des États-Unis en passant par l’Europe jusqu’à la côte pacifique de l’Asie. L’étymologie est complexe puisqu’en France, l’érable vient de l’ancien français acerabulus, composé d’acer, qui signifie aiguille en latin, et bulus qui viendrait du gaulois abalo pour pomme.
-
-Les érables mesurent généralement entre 10 et 45 m de hauteur. Leurs feuilles sont nervurées et arborent leurs couleurs d’automne au mois d’octobre, de l’orange au violet, et leur feuillage est dense. Les fleurs d’érable, également très colorées (rouge, orange, jaune, vert), comportent 4 à 5 pétales très courts, voire inexistants. Après sa floraison, l’érable produit des fruits, qui sont appelés samare. Toujours groupés par deux, ils forment des hélices, ce qui permet à la graine qu’elle contient d’être facilement disséminée par le vent.
-La feuille d’érable est bien évidemment le symbole du Canada, mais l’érable de manière générale a aussi d’autres symboliques. Il est associé à Phobos, le dieu grec de la peur panique. Son bois était utilisé pour fabriquer les lances de l’armée romaine, et enfin sa fleur symboliserait le sens de l’observation et la réserve."?>
-    
-	<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
-
+  </body>
+</html>
