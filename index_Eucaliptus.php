32605dc087067e5020cadc04006e6fe841f5f9d8
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 13:38:29 +0100

    eucalytus

--- index_Eucaliptus.php
+++ index_Eucaliptus.php
@@ -1,24 +1,21 @@
-  <html>
-    <?php include "head.inc.php" ?>
+<html>
+  <?php include "head.inc.php" ?>
   <body>
-    <div class="row">
     <?php include "entete.inc.php" ?>
-
-      <div class="col s12"><h1><?php echo "L'Eucaliptus" ?></h1></div>
-      <div class="col s12"><h2></h2></div>
-	  <div class="col s6">
-		</div>
-      <div class="col s6"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>L'Eucaliptus</h1></div>
+        <div class="col s4">
+          <img src="eucalyptus_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>L’eucalyptus est un genre de plantes vivaces, au feuillage persistant de la famille des Myrtaceae. Natif d’Australie, de Tasmanie et des îles environnantes, l’eucalyptus peut se cultiver partout dans le monde, dans les régions tempérées. Les espèces les plus impressionnantes que l’on trouve en Tasmanie peuvent atteindre jusqu’à 90 mètres de hauteur avec un circonférence de 7.5 mètres.</p>
+          <p>L’eucalyptus se cultive dans des endroits très ensoleillés, mais certaines espèces tolèrent des endroits partiellement ensoleillés. Cette plante s’adapte bien à une variété de sols, secs ou légèrement mouillés, tant que le sol est bien drainé et fertile ; il peut également être argileux ou composé d’humus ou de terreau, voire calcaire.</p>
+          <p>L’eucalyptus est une plante sensible au froid, elle ne tolère pas des températures en-dessous de 10 degrés. Elle peut donc se cultiver en France en extérieur dans la région méditerranéenne ou sur le pourtour atlantique. Pour les autres régions, on la cultive alors en intérieur, surtout pendant les mois les plus froids.</p>
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
     </div>
-
-    <?php echo "L’eucalyptus est un genre de plantes vivaces, au feuillage persistant de la famille des Myrtaceae. Natif d’Australie, de Tasmanie et des îles environnantes, l’eucalyptus peut se cultiver partout dans le monde, dans les régions tempérées. Les espèces les plus impressionnantes que l’on trouve en Tasmanie peuvent atteindre jusqu’à 90 mètres de hauteur avec un circonférence de 7.5 mètres.
-
-L’eucalyptus se cultive dans des endroits très ensoleillés, mais certaines espèces tolèrent des endroits partiellement ensoleillés. Cette plante s’adapte bien à une variété de sols, secs ou légèrement mouillés, tant que le sol est bien drainé et fertile ; il peut également être argileux ou composé d’humus ou de terreau, voire calcaire.
-
-L’eucalyptus est une plante sensible au froid, elle ne tolère pas des températures en-dessous de 10 degrés. Elle peut donc se cultiver en France en extérieur dans la région méditerranéenne ou sur le pourtour atlantique. Pour les autres régions, on la cultive alors en intérieur, surtout pendant les mois les plus froids."?>
-    
-	<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
-
+  </body>
+</html>
