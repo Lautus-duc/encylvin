8f3bd33aeb552553215773b39cc51bc77f4bc667
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 13:24:41 +0100

    sapin

--- index_Sapin.php
+++ index_Sapin.php
@@ -1,25 +1,23 @@
-  <html>
-    <?php include "head.inc.php" ?>
+<html>
+  <?php include "head.inc.php" ?>
   <body>
-    <div class="row">
     <?php include "entete.inc.php" ?>
-
-      <div class="col s12"><h1><?php echo "Le sapin" ?></h1></div>
-      <div class="col s12"><h2></h2></div>
-	  <div class="col s6">
-		</div>
-      <div class="col s6"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>Le sapin</h1></div>
+        <div class="col s4">
+        <img src="sapin_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>Le sapin est un genre de plantes vivaces, au feuillage persistant de la famille des Pinaceae. Originaire des régions tempérées de l’hémisphère nord, le sapin pousse partout dans le monde dans les régions tempérées ou froides.</p>
+          <p>Le sapin présente la particularité d’avoir des aiguilles persistantes qui peuvent durer plus de 10 ans avant de tomber. Certaines espèces peuvent atteindre jusqu’à 30 mètres de hauteur et vivre plusieurs siècles.  Le sapin peut être cultivé dans une grande variété de sols, qu’ils soient argileux, sableux, calcaires, caillouteux ou composés d’humus. Il tolère également des sols humides et ne nécessite que peu d’entretien.</p>
+          <p>Le sapin peut être cultivé partout en France ; c’est une plante très résistante au froid qui peut être planté en pleine terre ou dans des bacs. Toutefois, il est préférable de le cultiver en pleine terre, étant donné la taille qu’il peut atteindre. Une bonne solution est d’utiliser des pots/bacs durant les premières années, puisque c’est une plante à croissance lente, puis de le transplanter en pleine terre pour les années suivantes.</p>
+          <p>Le sapin préfère une exposition mi-ensoleillée à ensoleillée car en situation ombragé sa croissance serait encore plus lente." </p>
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
     </div>
-    
-    <?php echo "Le sapin est un genre de plantes vivaces, au feuillage persistant de la famille des Pinaceae. Originaire des régions tempérées de l’hémisphère nord, le sapin pousse partout dans le monde dans les régions tempérées ou froides.
-Le sapin présente la particularité d’avoir des aiguilles persistantes qui peuvent durer plus de 10 ans avant de tomber. Certaines espèces peuvent atteindre jusqu’à 30 mètres de hauteur et vivre plusieurs siècles.  Le sapin peut être cultivé dans une grande variété de sols, qu’ils soient argileux, sableux, calcaires, caillouteux ou composés d’humus. Il tolère également des sols humides et ne nécessite que peu d’entretien.
-
-Le sapin peut être cultivé partout en France ; c’est une plante très résistante au froid qui peut être planté en pleine terre ou dans des bacs. Toutefois, il est préférable de le cultiver en pleine terre, étant donné la taille qu’il peut atteindre. Une bonne solution est d’utiliser des pots/bacs durant les premières années, puisque c’est une plante à croissance lente, puis de le transplanter en pleine terre pour les années suivantes.
-
-Le sapin préfère une exposition mi-ensoleillée à ensoleillée car en situation ombragé sa croissance serait encore plus lente." ?>
-
-<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
+  </body>
+</html>
 
