4b0b3f2bf91de523bb7bd8f4d46f42eb3aad18da
Lautus-duc <tg3101tg@gmail.com>
2022-01-08 14:05:12 +0100

    thé des jésuites

--- index_Thé_des_jésuites.php
+++ index_Thé_des_jésuites.php
@@ -1,35 +1,27 @@
 <html>
-    <?php include "head.inc.php" ?>
+  <?php include "head.inc.php" ?>
   <body>
-    <div class="row">
     <?php include "entete.inc.php" ?>
-
-    <div class="row">
-      <div class="col s12"><h1><?php echo 'Le thé des jésuites' ?></h1></div>
-      <div class="col s12"><h2></h2></div>
-	  <div class="col s6">
-		</div>
-      <div class="col s6"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12"><h1>Le thé des jésuites</h1></div>
+        <div class="col s4">
+          <img src="the_des_ jesuites_1.jpg" class="responsive-img">
+        </div>
+        <div class="col s8">
+          <p>Le Maté vert, ou thé des jésuites, est une plante d'Amérique du Sud, aux vertus nombreuses.</p>
+          <p>Cette plante de la famille du Houx, est riche en caféine et donc stimulante.
+          Elle a un nombre impressionnant de composant, et est donc à juste titre appelé thé magique d'Amérique du Sud. Parmi ses composants, certains auraient la faculter d'aider à maigrir, notamment en aidant la perte de poids pendant un régime.</p>
+          <p>Origine du Maté :</p>
+          <p>Le maté provient d'une plante d'Amérique du sud, et très populaire dans cette région.
+          Il y est traditionnellement bu à l'aide d'une calebasse (récipient) et d'une bombilla (paille). On l'appelle Yerba Maté ou thé des jésuites.</p>
+          <p>Son développement en Europe est récent, lié à ses nombreuses vertus et propriétés, et principalement car il aide à perdre du poids.</p>
+          <p>Vertus du maté :</p>
+          <p>Le maté a de nombreuses vertus et bienfaits pour l'organisme. Ceci est dû au fait qu'il s'agit de la plante ayant le plus de composants connus à ce jour, vitamines et minéraux confondus.</p>
+        </div> 
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+      </div>
     </div>
-
-    <?php echo "Le Maté vert, ou thé des jésuites, est une plante d'Amérique du Sud, aux vertus nombreuses.
-
-Cette plante de la famille du Houx, est riche en caféine et donc stimulante.
-
-Elle a un nombre impressionnant de composant, et est donc à juste titre appelé thé magique d'Amérique du Sud. Parmi ses composants, certains auraient la faculter d'aider à maigrir, notamment en aidant la perte de poids pendant un régime.
-Origine du Maté :
-
-Le maté provient d'une plante d'Amérique du sud, et très populaire dans cette région.
-
-Il y est traditionnellement bu à l'aide d'une calebasse (récipient) et d'une bombilla (paille). On l'appelle Yerba Maté ou thé des jésuites.
-
-Son développement en Europe est récent, lié à ses nombreuses vertus et propriétés, et principalement car il aide à perdre du poids.
-Vertus du maté :
-
-Le maté a de nombreuses vertus et bienfaits pour l'organisme. Ceci est dû au fait qu'il s'agit de la plante ayant le plus de composants connus à ce jour, vitamines et minéraux confondus."?>
-
-	<!--JavaScript at end of body for optimized loading-->
-      <script type="text/javascript" src="js/materialize.min.js"></script>
-    </body>
-  </html>
-
+  </body>
+</html>
