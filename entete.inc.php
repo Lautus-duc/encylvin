d17694053787e6a3c67518418468955fd34f39eb
Lautus-duc <tg3101tg@gmail.com>
2022-03-22 18:02:02 +0100

    avancement menu

--- entete.inc.php
+++ entete.inc.php
@@ -11,12 +11,12 @@
       <a class='dropdown-trigger btn' href='#' data-target='dropdown1'>Menu !</a>
       <!-- Dropdown Structure -->
       <ul id='dropdown1' class='dropdown-content'>
-        <li><a href="#!">one</a></li>
-        <li><a href="#!">two</a></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#!">Reference</a></li>
         <li class="divider" tabindex="-1"></li>
         <li><a href="#!">three</a></li>
         <li><a href="#!"><i class="material-icons">view_module</i>four</a></li>
-        <li><a href="#!"><i class="material-icons">cloud</i>five</a></li>
+        <li><a href="#!"><i class="material-icons">tree</i>five</a></li>
       </ul>
     </div>
   </div> 
